a4e6be1430f8c127898133cbe349bd475db4c142
ROJA-KUMAR <137876714+ROJA-KUMAR@users.noreply.github.com>
2024-04-02 13:30:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
 import streamlit as st
 import pyttsx3
+import base64
 
 def text_to_speech(text):
     # Initialize the text-to-speech engine
     engine = pyttsx3.init()
-
+    
     # Set properties (optional)
     engine.setProperty('rate', 150)  # Speed percent (can go over 100)
     engine.setProperty('volume', 0.9)  # Volume 0-1
-
-    # Synthesize speech
-    engine.save_to_file(text, 'output.mp3')
-
-    # Run the engine (only needed if not running inside a GUI)
+    
+    # Save speech to bytes
+    temp_file = "output2.mp3"
+    engine.save_to_file(text, temp_file)
     engine.runAndWait()
+    
+    # Read the audio file as bytes
+    with open(temp_file, "rb") as file:
+        audio_bytes = file.read()
+
+    return audio_bytes
 
 def main():
     st.title("Text-to-Speech App")
     text = st.text_input("Enter the text you want to convert to speech")
     if st.button("Convert to Speech"):
         if text:
-            text_to_speech(text)
-            st.audio('output.mp3', format='audio/mp3')
+            audio_bytes = text_to_speech(text)
+            st.audio(audio_bytes, format='audio/mp3', start_time=0)
 
 if __name__ == "__main__":
     main()
-
